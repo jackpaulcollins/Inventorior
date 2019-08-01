c699de66367c0e3fd954344a28635f413633efdf
Jack Collins <jackpaulcollins@gmail.com>
2019-08-01 00:39:59 -0700

    added basic styling and layout of navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from './components/Nav'
 import ItemField from './components/ItemField'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <ItemField />
     </div>
   );
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -11,7 +11,6 @@ class Item extends React.Component {
   render () {
     return (
       <div className='item-box'>
-        jsjs
       </div>
     )
   }
