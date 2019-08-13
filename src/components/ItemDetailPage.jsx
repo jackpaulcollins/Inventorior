afbc43dc08fe63da9de9ad219afbbdf21ee418da
Jack Collins <jackpaulcollins@gmail.com>
2019-08-13 15:52:35 -0700

    redirect user to home after deleting item

--- src/components/ItemDetailPage.jsx
+++ src/components/ItemDetailPage.jsx
@@ -19,12 +19,6 @@ class ItemDetailPage extends React.Component {
     this._isMounted = false;
   }
 
-  // componentWillUpdate() {
-  //   if (this._isMounted) {
-  //     this.fetchItemData();
-  //   }
-  // }
-
   fetchItemData() {
     if (this._isMounted) {
       this.getItemPromise().then((data) => {
@@ -34,13 +28,14 @@ class ItemDetailPage extends React.Component {
   }
 
   deleteItemFromDatabase (id) {
-    console.log(id)
     const db = firebase.firestore();
       db.collection("items").doc(`${id}`).delete().then(function() {
         console.log("Document successfully deleted!");
     }).catch(function(error) {
         console.error("Error removing document: ", error);
     });
+    alert('Item Succesfully Deleted')
+    this.props.history.push('/')
   }
 
   getItemPromise() {
@@ -68,7 +63,7 @@ class ItemDetailPage extends React.Component {
           <h1>Item Title: {this.state.data ? this.state.data.itemName : ''}</h1>
           <p>Item count: {this.state.data ? this.state.data.itemQuantity : ''}</p>
           <p>Item Location: {this.state.data ? this.state.data.itemLocation : ''}</p>
-          <button onClick={() => this.deleteItemFromDatabase(this.props.match.params.id)}>Delete Item</button>
+          <button onClick={() => { if (window.confirm('Are you sure you want to delete this item?'))this.deleteItemFromDatabase(this.props.match.params.id)}}>Delete Item</button>
         </div>
       </div>
     )
